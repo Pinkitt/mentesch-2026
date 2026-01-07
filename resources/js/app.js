12f755832f6690bcfec1ffffb60f3839e972bc18
Pinkitt <113088229+Pinkitt@users.noreply.github.com>
2026-01-07 22:26:53 +0100

    Fix the file again

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 10000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
