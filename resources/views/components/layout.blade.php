173e5693a1bebfd1bdfd28376f9d3aef95444763
Pinkitt <113088229+Pinkitt@users.noreply.github.com>
2026-01-08 16:39:53 +0100

    color for the website and redoing the routes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> 
+<html lang="hu">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,33 +10,35 @@
   <title>Kezdőlap</title>
 </head>
 
-<body class="flex flex-col min-h-screen bg-gray-950 text-white">
+<body class="flex flex-col min-h-screen text-white">
 
-
-  <nav class="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+  <nav class="relative bg-[#49ab6d]/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="flex items-center">
           <img src="logo_ver_2.png" alt="Mentesch logo" class="h-20 w-auto" />
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4 ml-4">
-              <a href="/" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Kezdőlap</a>
-              <a href="/allergenek" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Allergének</a>
-              <a href="/etteremkereso" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Étterem kereső</a>
+              <a href="/" aria-current="page" class="rounded-md bg-[#49aba8]/50 px-3 py-2 text-sm font-medium text-white">Kezdőlap</a>
+              <a href="/allergens" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Allergének</a>
+              <a href="/restaurantsearch" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Étterem kereső</a>
             </div>
           </div>
         </div>
         <div class="flex items-center space-x-4">
-            <a href="/login" class="hover:text-indigo-400">Bejelentkezés</a>
+            <a href="/bejelentkezes" class="hover:text-indigo-400">Bejelentkezés</a>
         </div>
       </div>
     </div>
   </nav>
-  <header class="relative bg-white shadow-sm">
-  <h1 class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-             text-white text-9xl font-bold drop-shadow-lg texto">
-    {{$heading}}
-  </h1>
+
+  <header class="relative shadow-sm">
+    <div class="relative inline-block w-full px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                 text-white text-9xl font-bold drop-shadow-lg texto">
+        {{$heading}}
+      </h1>
+    </div>
   </header>
 
   <main class="flex-grow">
@@ -47,7 +49,7 @@
     </div>
   </main>
   
-  <footer class="bg-gray-900 text-gray-300 px-8 py-6">
+  <footer class="bg-[#40694f] text-gray-300 px-8 py-6">
     <div class="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
       <div>
         <h2 class="text-xl font-semibold text-white">Mentesch</h2>
